e991f59863a87cf9e4ffbd7ed4976dd1c0ecf45a
Hugo STAEDELIN <hugostaedelin68@gmail.com>
2022-09-30 15:14:15 +0200

     

---
+++ src/main/java/fr/hstaedelin/electoexpo/ElectoexpoApplication.java
@@ -0,0 +1,13 @@
+package fr.hstaedelin.electoexpo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectoexpoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectoexpoApplication.class, args);
+	}
+
+}
